f4b96fd46cedcb429e65a3ff05496040dc64987f
yuik <48705423+yu1k@users.noreply.github.com>
2020-03-10 16:24:06 +0900

    04 : 文字列

---
+++ n-yobikou/1-beginner-level/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char ch = 'A';
+    char str[] = "きた〜！！"; //文字列の場合はダブルクウォーテーションを使用
+
+    printf("ch=%c\n", ch);
+    printf("str=%s\n", str);
+    return 0;
+}
