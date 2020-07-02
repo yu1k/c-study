0118b2b56626a861085a1d0633b015984c85beb9
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-02 16:23:34 +0900

    04 : 文字列

--- n-yobikou/1-beginner-level/4.c
+++ n-yobikou/1-beginner-level/4.c
@@ -5,6 +5,10 @@ int main(){
     char ch = 'A';
     char str[] = "きた〜！！"; //文字列の場合はダブルクウォーテーションを使用
 
+    //ch=何を表しているか
+    //str=何を表しているか
+    //%c = char のこと
+    //%s = string のこと
     printf("ch=%c\n", ch);
     printf("str=%s\n", str);
     return 0;
