315708be58a543fc63a1e2ab0b6a8505e30de917
yu1k <48705423+yu1k@users.noreply.github.com>
2020-07-02 16:35:22 +0900

    05 : 式と演算子

---
+++ n-yobikou/1-beginner-level/5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int ans;
+    
+    ans = 20 + 10;
+    printf("ans=%d\n", ans);
+    ans = 20 - 10;
+    printf("ans=%d\n", ans);
+    ans = 20 * 10;
+    printf("ans=%d\n", ans);
+    ans = 20 / 10;
+    printf("ans=%d\n", ans);
+    ans = 20 % 10;
+    printf("ans=%d\n", ans);
+    return 0;
+}
